13c10e5e1f10da09b998aa7d37731fa9bb39bc1b
moonlust-studio <thegioigiay360jp@gmail.com>
2025-06-25 18:45:11 +0900

    🐛 Fix import path in art.tsx for Cloudflare build

--- pages/art.tsx
+++ pages/art.tsx
@@ -1,5 +1,5 @@
 // ✅ File: pages/art.tsx
-import '@/styles/globals.css';
+//import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import Head from 'next/head';
