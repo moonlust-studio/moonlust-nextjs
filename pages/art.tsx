0156e8d591412e4e4d544d664535194a5dc38bf9
moonlust-studio <thegioigiay360jp@gmail.com>
2025-06-25 17:53:16 +0900

    🐛 Fix import i18n config in art.tsx for Cloudflare

--- pages/art.tsx
+++ pages/art.tsx
@@ -2,7 +2,7 @@ import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 import { appWithTranslation } from 'next-i18next';
 import Head from 'next/head';
-import nextI18NextConfig from '../../next-i18next.config.js';
+import nextI18NextConfig from '@/next-i18next.config'; // ✅ Dùng alias `@/`
 
 function App({ Component, pageProps }: AppProps) {
   return (
