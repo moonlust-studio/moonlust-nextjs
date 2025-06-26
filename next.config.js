c2264c3831f1ba6144dfcc73d86f57fd85579c84
moonlust-studio <thegioigiay360jp@gmail.com>
2025-06-26 19:08:56 +0900

    ✨ fix art,css

--- next.config.js
+++ next.config.js
@@ -1,11 +1,8 @@
-// ✅ Đây mới là nội dung đúng của `next.config.js`
-
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   i18n: {
     locales: ['vi', 'en', 'ja'],
     defaultLocale: 'vi',
-    localeDetection: false, // ✅ Tắt tự động chuyển locale
+    localeDetection: true,
   },
   reactStrictMode: true,
 };
